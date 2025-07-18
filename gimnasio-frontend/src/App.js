ce0a722b18499eac700e48322bf08700f698c190
thefnjb <Favio@gmail.com>
2025-07-18 14:19:29 -0500

    Inicialización de mi proyecto

---
+++ gimnasio-frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-red-700 dark:text-sky-400">
+      Bienvenido al gimnasio(Sparta)
+    </div>
+  );
+}
+
+export default App;
