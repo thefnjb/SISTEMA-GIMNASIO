03b69a6015f98d2e5f0f736c1cae317ea53b41f0
Favio Coronado <coronadozapatafavioalexander@gmail.com>
2025-09-22 08:53:07 -0500

    Cambios de 22-09-2025

--- gimnasio-frontend/src/components/Pdf/BotonpdfMensual.js
+++ gimnasio-frontend/src/components/Pdf/BotonpdfMensual.js
@@ -1,11 +1,14 @@
 "use client"
 
-import {Autocomplete, AutocompleteItem} from "@heroui/react"
-import api from "../../utils/axiosInstance";
+import { Autocomplete, AutocompleteItem } from "@heroui/react"
+import api from "../../utils/axiosInstance"
 
 const currentYear = new Date().getFullYear()
-const years = Array.from({ length: currentYear - 2000 + 1 }, (_, i) => {
-  const year = currentYear - i
+const startYear = 2000
+const endYear = currentYear + 5
+
+const years = Array.from({ length: endYear - startYear + 1 }, (_, i) => {
+  const year = startYear + i
   return {
     label: `Año ${year}`,
     key: year.toString(),
@@ -14,23 +17,23 @@ const years = Array.from({ length: currentYear - 2000 + 1 }, (_, i) => {
 
 export default function Añosselector() {
   const handleDownload = async (year) => {
-    if (!year) return;
+    if (!year) return
     try {
       const response = await api.get(`/pdfdia/reporte-mensual`, {
         params: { year },
-        responseType: 'blob',
-      });
+        responseType: "blob",
+      })
 
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', `reporte-${year}.pdf`);
-      document.body.appendChild(link);
-      link.click();
-      link.parentNode.removeChild(link);
-      window.URL.revokeObjectURL(url);
+      const url = window.URL.createObjectURL(new Blob([response.data]))
+      const link = document.createElement("a")
+      link.href = url
+      link.setAttribute("download", `reporte-${year}.pdf`)
+      document.body.appendChild(link)
+      link.click()
+      link.parentNode.removeChild(link)
+      window.URL.revokeObjectURL(url)
     } catch (error) {
-      console.error("Error downloading the file:", error);
+      console.error("Error downloading the file:", error)
     }
   }
 
@@ -38,7 +41,7 @@ export default function Añosselector() {
     <Autocomplete
       className="max-w-xs"
       defaultItems={years}
-      label="Selecciona un año para descargar "
+      label="Selecciona un año para descargar"
       onSelectionChange={(key) => handleDownload(key)}
     >
       {(year) => <AutocompleteItem key={year.key}>{year.label}</AutocompleteItem>}
