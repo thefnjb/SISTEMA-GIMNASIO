faa017f336a545786dfb9fefe3c92d38eafc453a
Favio Coronado <coronadozapatafavioalexander@gmail.com>
2025-09-23 08:53:47 -0500

    Eliminar el título "Ingresos" del componente Ingresos

--- gimnasio-frontend/src/Pages/Paginas/Ingresos.js
+++ gimnasio-frontend/src/Pages/Paginas/Ingresos.js
@@ -19,7 +19,6 @@ function Ingresos() {
       }}
     >
       <div className="flex items-center justify-between mb-6">
-        <h1 className="text-2xl font-bold"> Ingresos</h1>
         <Añosselector />
       </div>
 
